3a22b7b6f6e7a2c0c5db94bce11a276c843fcbfd
Txgxr <redblacktige@gmail.com>
2022-12-08 09:31:25 -0800

    Changed Colors, Added Edit and Delete Buttons

--- main.py
+++ main.py
@@ -11,30 +11,51 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
 import os
+from kivy.properties import StringProperty
+from kivy.lang import Builder
+
+Builder.load_string("""
+<EditIconButton>:
+    canvas:
+        Rectangle:
+            source:self.editIcon
+            pos: self.center_x-20, self.center_y-20
+            size: 40,40
+
+<TrashIconButton>:
+    canvas:
+        Rectangle:
+            source:self.trashIcon
+            pos: self.center_x-20, self.center_y-20
+            size: 40,40
+""")
+
 
 if not os.path.exists('C:\github\intro to programming\ITPFinalProject\Resources\Splits'):
     os.umask(0)
     os.mkdir('C:\github\intro to programming\ITPFinalProject\Resources\Splits', 0o777)
     os.chmod('C:\github\intro to programming\ITPFinalProject\Resources\Splits', 0o777)
 
-class Split():
-    def __init__(self, splitName):
-        self.splitName = splitName
+class EditIconButton(Button):
+    editIcon = StringProperty('C:\github\intro to programming\ITPFinalProject\Resources\Images\pencil_icon.png')
+
+class TrashIconButton(Button):
+    trashIcon = StringProperty('C:\github\intro to programming\ITPFinalProject\Resources\Images\\trash_icon.png')
 
 class StartScreen(Screen, FloatLayout):
 
     def __init__(self, **kwargs):
         
         super(StartScreen, self).__init__(**kwargs)
-        self.splitsButton = Button(text="Splits", size_hint=(.2,.1), pos_hint={'center_x':.5, 'center_y':.5}, background_color=(0.2,0.2,1,0.8))
+        self.splitsButton = Button(text="Splits", size_hint=(.2,.1), pos_hint={'center_x':.5, 'center_y':.5}, background_color=(0.294,0,1,0.509))
         self.add_widget(self.splitsButton)
         self.splitsButton.bind(on_press=self.toSplitsScreen)
 
-        self.optionsButton = Button(text='Options', size_hint=(.2,.1), pos_hint={'center_x':.5, 'center_y':.38}, background_color=(0.2,0.2,1,0.8))
+        self.optionsButton = Button(text='Options', size_hint=(.2,.1), pos_hint={'center_x':.5, 'center_y':.38}, background_color=(0.294,0,1,0.509))
         self.add_widget(self.optionsButton)
         self.optionsButton.bind(on_press=self.toOptionsScreen)
 
-        self.exitButton = Button(text="Exit", size_hint=(.2,.1), pos_hint={'center_x':.5, 'center_y':.26}, background_color=(0.2,0.2,1,0.8))
+        self.exitButton = Button(text="Exit", size_hint=(.2,.1), pos_hint={'center_x':.5, 'center_y':.26}, background_color=(0.294,0,1,0.509))
         self.add_widget(self.exitButton)
         self.exitButton.bind(on_press=MySplit().stop)
 
@@ -62,11 +83,11 @@ class SplitsScreen(Screen, FloatLayout):
     
     def __init__(self, **kwargs):
         super(SplitsScreen, self).__init__(**kwargs)
-        self.newSplitButton = Button(text='New Split', size_hint=(.2,.1), pos_hint={'x':.75, 'y':.75}, background_color=(0.2,0.2,1,0.8))
+        self.newSplitButton = Button(text='New Split', size_hint=(.2,.1), pos_hint={'x':.75, 'y':.75}, background_color=(0.294,0,1,0.509))
         self.add_widget(self.newSplitButton)
         self.newSplitButton.bind(on_press=self.splitNamePopup)
 
-        self.backToMenuButton = Button(text='Back', size_hint=(.2,.1), pos_hint={'x':.05, 'y':.75}, background_color=(0.2,0.2,1,0.8))
+        self.backToMenuButton = Button(text='Back', size_hint=(.2,.1), pos_hint={'x':.05, 'y':.75}, background_color=(0.294,0,1,0.509))
         self.add_widget(self.backToMenuButton)
         self.backToMenuButton.bind(on_press=self.toStartScreen)
 
@@ -78,14 +99,12 @@ class SplitsScreen(Screen, FloatLayout):
 
         for split in self.splitEntries:
             self.splitCount += 1
-            self.add_widget(Button(text=f'{os.path.splitext(split)[0]}', pos_hint={'center_x':.5,'center_y':.1*self.splitCount*1.5}, size_hint=(None, None), size=(450, 80), font_size=25, background_color=(0.1,0.1,1,0.5)))
+            self.add_widget(Button(text=f'{os.path.splitext(split)[0]}', pos_hint={'center_x':.5,'center_y':.75-(.1*(self.splitCount-1)*1.5)}, size_hint=(None, None), size=(450, 80), font_size=25, background_color=(0.294,0,1,0.509)))
+            self.add_widget(EditIconButton(pos_hint={'center_x':.15, 'center_y':.75-(.1*(self.splitCount-1)*1.5)}, size_hint=(None, None), size=(80,80), background_color=(0.294,0,1,0.509)))
+            self.add_widget(TrashIconButton(pos_hint={'center_x':.85, 'center_y':.75-(.1*(self.splitCount-1)*1.5)}, size_hint=(None, None), size=(80,80), background_color=(0.294,0,1,0.509)))
             print(os.path.splitext(split)[0])
             print(self.splitCount)
-        # self.splits = os.listdir('C:\github\intro to programming\ITPFinalProject\Resources\Splits')
-        # self.splitCount = 1
-        # for split in self.splits:
-        #     self.add_widget(self.add_widget(Button(text=f'{split}', pos_hint={'center_x':.5,'center_y':.1}, size_hint=(None, None), size=(450, 80), font_size=25, background_color=(0.1,0.1,1,0.5))))
-        #     self.splitCount += 1
+
 
     def toStartScreen(self, instance):
         sm.current='start'
@@ -104,7 +123,7 @@ class SplitsScreen(Screen, FloatLayout):
         splitNameBox.add_widget(splitPopupNameLabel)
         splitNameBox.add_widget(closeSplitPopupButton)
 
-        splitNamePopup = Popup(title='Creating New Split',content=splitNameBox, size_hint=(None, None), size=(300,250), auto_dismiss=False, background_color=(0.2,0.2,1,1))
+        splitNamePopup = Popup(title='Creating New Split',content=splitNameBox, size_hint=(None, None), size=(300,250), auto_dismiss=False, background_color=(0.294,0,1,1))
         closeSplitPopupButton.bind(on_press=splitNamePopup.dismiss)
         splitPopupEnterButton.bind(on_press=lambda x:self.createSplit(splitPopupTextInput.text))
         splitPopupEnterButton.bind(on_press=splitNamePopup.dismiss)
@@ -114,7 +133,12 @@ class SplitsScreen(Screen, FloatLayout):
     def createSplit(self, splitName):
         if not os.path.exists(f'C:\github\intro to programming\ITPFinalProject\Resources\Splits\{splitName}.txt') and self.splitCount < 5:
             splitNameFile = open(f'C:\github\intro to programming\ITPFinalProject\Resources\Splits\{splitName}.txt', 'w')
-            self.add_widget(Button(text=f'{splitName}', pos_hint={'center_x':.5,'center_y':.1*self.splitCount*1.5}, size_hint=(None, None), size=(450, 80), font_size=25, background_color=(0.1,0.1,1,0.5)))
+            self.add_widget(Button(text=f'{splitName}', pos_hint={'center_x':.5,'center_y':.75-(.1*self.splitCount*1.5)}, size_hint=(None, None), size=(450, 80), font_size=25, background_color=(0.294,0,1,0.509)))
+            editIconButton = EditIconButton(pos_hint={'center_x':.15, 'center_y':.75-(.1*self.splitCount*1.5)}, size_hint=(None, None), size=(80,80), background_color=(0.294,0,1,0.509))
+            self.add_widget(editIconButton)
+            trashIconButton = TrashIconButton(pos_hint={'center_x':.85, 'center_y':.75-(.1*self.splitCount*1.5)}, size_hint=(None, None), size=(80,80), background_color=(0.294,0,1,0.509))
+            self.add_widget(trashIconButton)
+
             splitNameFile.close()
             self.splitCount += 1
             lambda x:self.checkSplitFiles()
@@ -128,7 +152,7 @@ class OptionsScreen(Screen, FloatLayout):
         self.optionsLogo = Image(source='C:\github\intro to programming\ITPFinalProject\Resources\Images\options_logo.png',pos_hint={'center_x':.5,'center_y':.8})
         self.add_widget(self.optionsLogo)
 
-        self.backToMenuButton = Button(text='Back', size_hint=(.2,.1), pos_hint={'x':.05, 'y':.75}, background_color=(0.2,0.2,1,0.8))
+        self.backToMenuButton = Button(text='Back', size_hint=(.2,.1), pos_hint={'x':.05, 'y':.75}, background_color=(0.294,0,1,0.509))
         self.add_widget(self.backToMenuButton)
         self.backToMenuButton.bind(on_press=self.toStartScreen)
 
